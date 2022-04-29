8d6ae49c88d3a91c445ee7dce7b1f4a5d81f8087
misakabili1 <97622268+misakabili1@users.noreply.github.com>
2022-04-30 01:26:52 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
 echo root:test | sudo chpasswd root
-sudo sed -i "s/^#\?Port.*/Port $sshport/g" /etc/ssh/sshd_config;
+sudo sed -i "s/^#\?Port.*/Port 22/g" /etc/ssh/sshd_config;
 sudo sed -i "s/^#\?PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config;
 sudo sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
 sudo service ssh restart
